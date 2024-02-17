cd4484fd069fb7d6d30d51c4a88cac3dbfb86013
alejandra2310 <156863657+alejandra2310@users.noreply.github.com>
2024-02-16 19:34:14 -0600

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int miArray[] = (25, 18, 18, 28, 22);
+int n- sizeof(miArray)/sizeof(miArray[0]);
+std::cout << "Array original: ";
+for (int i=0; i < n; i++)
+std::cout << miArray[i] <<
+bubbleSort(miArray, n):
+std::cout << "InArray ordenado: ";
+for (int i =0;i < n; it)
+std::cout << miArray[i] <
+}
+return 
